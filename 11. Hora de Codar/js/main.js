db7e3d0d4844ace5f810d26119d6b8532fd716b5
Matheus <matheusvsn2005@gmail.com>
2023-03-08 12:25:00 -0300

    08/03/2023

--- 11. Hora de Codar/js/main.js
+++ 11. Hora de Codar/js/main.js
@@ -1,22 +1,32 @@
-const Perguntas = {
-    1: {
-        Pergunta: `Escreva um programa em Javascript e crie uma variável chamada "nomeDoCarro" e atribua-a um valor "Fusca". Exiba-a ao usuário.`,
-        Interacao: {
-            1: {
-                element: "input",
-                type: "input",
-                class: "form__field",
-                placeholder: "Digite o nome do carro",
-                name: "NomeDoCarro",
-                id: "NomeDoCarro",
-                required: true
-            },
-        }
+function generateInputBox(Pergunta, Id, RequiredState) {
+    return {
+        element: "input",
+        type: "input",
+        class: "form__field",
+        placeholder: Pergunta,
+        name: Id,
+        id: Id,
+        required: RequiredState
+    }
+}
+
+function generateInputResult(id) {
+    return {
+        element: "p",
+        class: "form__result",
+        id: id
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
 
 function CONFIGURAR_ELEMENTO(Interacao) {
     const InteracaoElement = document.createElement(Interacao.element);
+
     if (Interacao.element == "input") {
         InteracaoElement.setAttribute("type", Interacao.type);
         InteracaoElement.setAttribute("class", Interacao.class);
@@ -27,12 +37,53 @@ function CONFIGURAR_ELEMENTO(Interacao) {
         if (Interacao.required) {
             InteracaoElement.setAttribute("required", Interacao.required);
         }
-    } else if (InteracaoElement == "text") {
-
+    } else if (Interacao.element == "p") {
+        InteracaoElement.setAttribute("class", Interacao.class);
+        InteracaoElement.innerText = "Resultado: ";
+        if (Interacao.id != null) {
+            InteracaoElement.setAttribute("id", Interacao.id)
+        }
     }
 
     return InteracaoElement
 }
+
+const Perguntas = {
+    1: {
+        Pergunta: `Escreva um programa em Javascript e crie uma variável chamada "nomeDoCarro" e atribua-a um valor "Fusca". Exiba-a ao usuário.`,
+        Id: "NomeDoCarro",
+        Resposta: "O nome do carro é: ",
+        Interacao: {
+            1: generateInputBox("Digite o nome do carro", "NomeDoCarro", true),
+            2: generateInputResult()
+        },
+        Tipo: 1
+    },
+    2: {
+        Pergunta: `Escreva um programa em Javascript em que o usuário informe o seu nome e exiba a mensagem "Olá, [NomeDoUsuario]".`,
+        Id: "NomeDoUsuario",
+        Resposta: "Olá, ",
+        Interacao: {
+            1: generateInputBox("Digite o seu nome", "NomeDoUsuario", true),
+            2: generateInputResult()
+        },
+        Tipo: 1
+    },
+    3: {
+        Pergunta: `Escreva um programa em Javascript em que o usuário informe o seu nome e em seguida o programa perguntará a idade do usuário. Agora o programa deve exibir a mensagem "Olá, [NomeDoUsuario], sua idade é [idade]".`,
+        Id: "NomeEIdade",
+        Interacao: {
+            1: generateInputBox("Digite o seu nome", "NomeDoUsuario2", true)
+        },
+        Tipo: 2
+    },
+    4: {
+        Pergunta: `Áreas de Figuras Planas`,
+        ImagemID: `https://lh6.googleusercontent.com/12VeEnZzYRUQqnRbEjxjJ3m2H6BxAU-oLQ0vuG08k-hpqXq5A36AZBSilhnFhT1Phe4S6ClFM6AHTHMTXkEf9BksDv2jI3bqHSH4nOR_tqbRntx4pmbdt3EzOLGrttrFtw=w321`,
+        Tipo: "IMAGEM"
+    }
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
     for (const key in Perguntas) {
@@ -57,41 +108,86 @@ document.addEventListener("DOMContentLoaded", function () {
         Line.setAttribute("class", "line");
         QuestionHeader.appendChild(Line);
 
+        // <picture>
+        if (Perguntas[key].Tipo == "IMAGEM") {
+            const PictureElement = document.createElement("picture");
+            const ImgElement = document.createElement("img");
+            ImgElement.src = Perguntas[key].ImagemID
+            PictureElement.appendChild(ImgElement)
+            QuestionHeader.appendChild(PictureElement)
+
+            // Decoração
+            // <div class="line">
+            const Line = document.createElement("div");
+            Line.setAttribute("class", "line");
+            QuestionHeader.appendChild(Line);
+        }
+
         // <div class="question-body">
         const QuestionBody = document.createElement("div");
         QuestionBody.setAttribute("class", "question-body");
         QuestionContainer.appendChild(QuestionBody);
 
         // Todas as interações
-        /*
-        for (const Index in Perguntas[key].Interacao) {
-            const Interacao = Perguntas[key].Interacao[Index];
-            const InteracaoElement = document.createElement(Interacao.element);
-            InteracaoElement.setAttribute("type", Interacao.type);
-            InteracaoElement.setAttribute("class", Interacao.class);
-            InteracaoElement.setAttribute("placeholder", Interacao.placeholder);
-            InteracaoElement.setAttribute("name", Interacao.name);
-            InteracaoElement.setAttribute("id", Interacao.id);
-
-            if (Interacao.required) {
-                InteracaoElement.setAttribute("required", Interacao.required);
-            }
-
-            QuestionBody.appendChild(InteracaoElement);
-        }
-        */
         for (const Index in Perguntas[key].Interacao) {
             const InteracaoElement = CONFIGURAR_ELEMENTO(Perguntas[key].Interacao[Index]);
             QuestionBody.appendChild(InteracaoElement);
         }
 
         // Botão de enviar
-        const SubmitButton = document.createElement("button");
-        const SubmitText = document.createTextNode("Enviar");
-        SubmitButton.appendChild(SubmitText);
-        SubmitButton.setAttribute("id", "submit");
-        QuestionContainer.appendChild(SubmitButton);
+        if (Perguntas[key].Tipo != "IMAGEM") {
+            const SubmitButton = document.createElement("button");
+            const SubmitText = document.createTextNode("Enviar");
+            SubmitButton.appendChild(SubmitText);
+            SubmitButton.setAttribute("id", "submit");
+            QuestionContainer.appendChild(SubmitButton);
 
+            // Evento de click
+            SubmitButton.addEventListener("click", function () {
+                let Tipo = Perguntas[key].Tipo;
+                if (Tipo == 1) {
+                    let Variavel = document.getElementById(Perguntas[key].Id).value;
+                    let ResultElement = document.getElementsByClassName("form__result")[key - 1];
+                    ResultElement.innerText = `${Perguntas[key].Resposta} ${Variavel}`
+                } else if (Tipo == 2) {
+                    // Checa se tem algum elemento perguntando a idade dele
+                    let PerguntaExistente = document.getElementById(`${Perguntas[key].Id} TEMPORARIO`)
+                    if (PerguntaExistente == null) {
+                        // Cria um novo elemento para o usuario colocar a idade dele
+                        let InteracaoElement = CONFIGURAR_ELEMENTO(generateInputBox("Digite a sua idade", `${Perguntas[key].Id} TEMPORARIO`, true));
+                        QuestionBody.appendChild(InteracaoElement);
+                    } else {
+                        let RespostaExistent = document.getElementById(`${Perguntas[key].Id} RESPOSTA TEMPORARIO`)
+                        if (RespostaExistent == null) {
+                            // Aqui nós ja temos a informação da idade e nome
+                            let Nome = document.getElementById("NomeDoUsuario2").value;
+                            let Idade = document.getElementById(`${Perguntas[key].Id} TEMPORARIO`).value;
+                            let ResultElement = CONFIGURAR_ELEMENTO(generateInputResult(`${Perguntas[key].Id} RESPOSTA TEMPORARIO`));
+                            ResultElement.innerText = `Olá ${Nome}, sua idade é ${Idade}`;
+
+                            QuestionBody.appendChild(ResultElement)
+
+                            // Coloca o texto de reiniciar a pergunta
+                            let NewText = document.createTextNode("Reiniciar")
+                            removeAllChildNodes(SubmitButton)
+                            SubmitButton.appendChild(NewText)
+                        } else {
+                            // Remove o texto de reinciar a perguntar e reinicia o questionario
+                            RespostaExistent.remove()
+                            let NewText = document.createTextNode("Enviar")
+                            removeAllChildNodes(SubmitButton)
+                            SubmitButton.appendChild(NewText)
+
+                            // Remove a pergunta da idade
+                            let ExistentAge = document.getElementById(`${Perguntas[key].Id} TEMPORARIO`)
+                            if (ExistentAge) {
+                                return ExistentAge.remove()
+                            }
+                        }
+                    }
+                }
+            })
+        }
         // quando tudo estiver pronto
         const mainElement = document.getElementById("main");
         mainElement.appendChild(QuestionContainer);
