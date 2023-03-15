6bf7113812543bb19d39afef52b124b14cd5d1b3
Matheus <matheusvsn2005@gmail.com>
2023-03-15 19:18:36 -0300

    15/03/2023
    -Meu computador crashou 3 vezes :pensive:

--- 13. Hora de Codar 3 - De volta ao código/js/bombTimer.js
+++ 13. Hora de Codar 3 - De volta ao código/js/bombTimer.js
@@ -2,10 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
     let CurrentCount = 30;
     document.write(`A bomba vai explodir em: ${CurrentCount}\n`);
 
-    while (CurrentCount > 0) {
-        setTimeout(function() {
-            CurrentCount -= 1
-            document.write(`A bomba vai explodir em: ${CurrentCount}\n`);
-        }, 1000)
-    }
+    
 })
