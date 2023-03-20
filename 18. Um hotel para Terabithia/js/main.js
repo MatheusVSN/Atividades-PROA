bde022332be3c6d0293a521c58731ab0c737e082
Matheus <matheusvsn2005@gmail.com>
2023-03-20 16:43:41 -0300

    20/03/2023

--- 18. Um hotel para Terabithia/js/main.js
+++ 18. Um hotel para Terabithia/js/main.js
@@ -1,137 +1,122 @@
-const NUMBER_INVALID_QUESTION = "Por favor, digite um número valido";
-
 const PASSWORD = 2678;
-const DIARIA_MAXIMA = 30;
+const NUMBER_INVALID_QUESTION = "Por favor, digite um número válido";
+const STRING_INVALID_QUESTION = "Por favor, preencha a caixa de texto";
+const COMANDO_PARAR_DIARIA = "PARE";
 const ANOS_PARA_DIARIA_GRATUITA = 6;
 const ANOS_PARA_MEIA_DIARIA = 60;
-const COMANDO_PARAR_DIARIA = "PARE";
-const QUANTIDADE_DE_LITROS = 42;
 
-var Hospedes = [];
-var Verificado = false;
-var NomeDoUsuario = "";
-
-function ask_for_string(argument) {
-    let PresumedString = prompt(argument);
-    while (!PresumedString) {
-        PresumedString = prompt(STRING_INVALID_QUESTION);
-    }
-
-    return PresumedString;
-}
-
-function check_for_password(Nome) {
-    let PresumedPassword = Number(prompt(`Olá ${Nome}, digite a senha do Hotel`));
-
-    return (PresumedPassword == PASSWORD);
-}
-
-function ask_for_number(argument) {
+function askForNumber(argument) {
     let PresumedNumber = Number(prompt(argument));
     while (isNaN(PresumedNumber)) {
-        PresumedNumber = Number(prompt(NUMBER_INVALID_QUESTION));
+        PresumedNumber = Number(prompt(`${NUMBER_INVALID_QUESTION}\n${argument}`));
     }
 
     return PresumedNumber;
 }
 
-var NomeDoHotel = ask_for_string(`Qual o nome você gostaria de colocar para o hotel?`);
-alert(`O nome do hotel é ${NomeDoHotel}`);
+function askForString(argument) {
+    let PresumedString = prompt(argument);
+    while (!PresumedString) {
+        PresumedString = prompt(`${STRING_INVALID_QUESTION}\n${argument}`);
+    }
+
+    return PresumedString;
+}
+
+
+var NomeDoHotel = prompt(`Qual o nome do hotel`);
+alert(`O nome do Hotel é ${NomeDoHotel}`);
+
+var NomeDoUsuario = prompt(`Qual o seu nome?`);
 
 function Main() {
-    if (!Verificado) {
-        NomeDoUsuario = ask_for_string(`Digite o seu nome de usuário`);
-        let Senha = check_for_password(NomeDoUsuario);
+    alert(`Bem vindo ao Hotel ${NomeDoHotel}, ${NomeDoUsuario}. É um imenso prazer ter você or aqui!`);
 
-        while (!Senha) {
-            Senha = check_for_password(NomeDoUsuario);
-        }
+    let Escolha = parseInt(prompt(`Selecione uma opção\n[1] Quantos quartos são?\n[2] Como soletra?\n[3] Com "S" ou com "Z"\n[4] Festa ou trabalho?\n[5] Hora de comer\n[6] Auditório para quantos?\n[7] Que horas você pode?\n[8] Álcool ou gasolina?\n[9] Ar puro, finalmente\n[10] Sair`));
 
-        Verificado = true;
-    }
-
-    alert(`Bem-vindo ao hotel ${NomeDoHotel}, ${NomeDoUsuario}. É um imenso prazer ter você por aqui!`);
-
-    let Opcao = parseInt(prompt(`Selecione uma opção\n[1] Reserva de quartos\n[2] Cadastro de Hóspedes\n[3] Abastecimento de carros\n[4] Sair`));
-
-    switch (Opcao) {
+    switch (Escolha) {
         case 1:
-            reserva_de_quartos();
+            QuantosQuartosSao();
             break;
         case 2:
-            cadastro_de_hospedes();
+            ComoSoletra();
             break;
         case 3:
-            abastecimento_de_carros();
+            ComSouZ();
             break;
         case 4:
-            sair();
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+            Sair();
             break;
         default:
-            error();
+            Error();
             break;
     }
 }
 
-function reserva_de_quartos() {
-    let ValorPadraoDiaria = ask_for_number(`Qual o valor padrão da diaria?`);
-
-    while (ValorPadraoDiaria < 0) {
-        alert(`Valor inválido, ${NomeDoUsuario}`);
-        ValorPadraoDiaria = ask_for_number(`Qual o valor padrão da diaria?`);
+function QuantosQuartosSao() {
+    let ValorPadraoDaDiaria = askForNumber(`Qual o valor padrão da diária?`);
+    while (ValorPadraoDaDiaria <= 0) {
+        ValorPadraoDaDiaria = askForNumber(`Valor inválido ${NomeDoUsuario}\nDigite um número maior que 0`);
     }
 
-    let Diarias = ask_for_number(`Quantas diárias serão necessárias?`);
-    while (Diarias < 0 || Diarias > DIARIA_MAXIMA) {
-        alert(`Valor inválido, ${NomeDoUsuario}`);
-        Diarias = ask_for_number(`Quantas diárias serão necessárias?`);
+    let Diarias = askForNumber(`Quantas diárias serão necessárias?`);
+    while (Diarias <= 0 || Diarias > 30) {
+        Diarias = askForNumber(`Valor inválido ${NomeDoUsuario}\nDigite um número entre 0 e 30`);
     }
 
-    let Calculo = ValorPadraoDiaria * Diarias;
-    alert(`O valor de 10 dias de hospedagem é de R$${Calculo}`);
+    let Preco = ValorPadraoDaDiaria * Diarias;
+    alert(`O valor de ${Diarias} dia(s) de hospedagem é de R$${Preco}`);
 
-    let NomeDoHospede = ask_for_string(`Qual o nome do hóspede?`);
-    let Confirmacao = ask_for_string(`${NomeDoUsuario}, você confirma a hospedagem para ${NomeDoHospede} por 10 dias? S/N`);
-    while (Confirmacao.toUpperCase() != "S" && Confirmacao.toUpperCase() != "N") {
-        alert(`Por favor, escolha entre\nS(sim) e N(não)`);
-        Confirmacao = ask_for_string(`${NomeDoUsuario}, você confirma a hospedagem para ${NomeDoHospede} por 10 dias? S/N`);
+    let NomeDoHospede = askForString(`Qual o nome do hospede?`);
+    let Confirmacao = askForString(`${NomeDoUsuario}, você confirma a hospedagem para ${NomeDoHospede} por ${Diarias}? S/N`);
+    Confirmacao = Confirmacao.toUpperCase();
+    while (Confirmacao != "S" && Confirmacao != "N") {
+        Confirmacao = askForString(`Por favor, escolha S ou N`);
     }
 
-    if (Confirmacao.toUpperCase() == "S") {
-        alert(`${NomeDoUsuario}, reserva efetuada para ${NomeDoHospede}. O valor total é de R$${Calculo}.`);
-        Main();
+    if (Confirmacao == "S") {
+        alert(`${NomeDoUsuario}, reserva efetuada para ${NomeDoHospede}. O valor total é de R$${Preco}`);
     } else {
         alert(`${NomeDoUsuario}, reserva não efetuada`);
-        Main();
     }
+
+    Main();
 }
 
-function cadastro_de_hospedes() {
+function ComoSoletra() {
     let ValorTotal = 0;
     let ValorMeia = 0;
     let ValorGratuidade = 0;
     let HospedeIndex = 0;
 
-    let ValorDaDiaria = ask_for_number(`Qual o valor da diária?`);
+    let ValorDaDiaria = askForNumber(`Qual o valor da diária?`);
     while (ValorDaDiaria <= 0) {
         alert(`Por favor, insira um número maior que 0`);
-        ValorDaDiaria = ask_for_number(`Qual o valor da diária?`);
+        ValorDaDiaria = askForNumber(`Qual o valor da diária?`);
     }
 
     function novo_cadastro() {
         HospedeIndex += 1;
-        let NomeDoHospede = ask_for_string(`[${HospedeIndex}º] Qual o nome do hóspede?`);
+        let NomeDoHospede = askForString(`[${HospedeIndex}º] Qual o nome do hóspede?`);
         if (NomeDoHospede != COMANDO_PARAR_DIARIA) {
-            let Idade = ask_for_number(`Qual a idade do hóspede?`);
-            if (Idade > ANOS_PARA_MEIA_DIARIA) {
+            let Idade = askForNumber(`Qual a idade do hóspede?`);
+            if (Idade <= ANOS_PARA_DIARIA_GRATUITA) {
+                ValorGratuidade += 1
+                alert(`${NomeDoHospede} cadastrado(a) com sucesso. ${NomeDoHospede} possui gratuidade`);
+            } else if (Idade >= ANOS_PARA_MEIA_DIARIA) {
                 ValorMeia += 1
                 ValorTotal += ValorDaDiaria / 2;
-            } else if (Idade >= ANOS_PARA_DIARIA_GRATUITA) {
-                ValorTotal += ValorDaDiaria
+                alert(`${NomeDoHospede} cadastrado(a) com sucesso. ${NomeDoHospede} paga meia`);
             } else {
-                ValorGratuidade += 1
+                ValorTotal += ValorDaDiaria
+                alert(`${NomeDoHospede} cadastrado(a) com sucesso.`);
             }
-            alert(`${NomeDoHospede} cadastrado com sucesso!`);
             novo_cadastro();
             return
         }
@@ -143,49 +128,75 @@ function cadastro_de_hospedes() {
     novo_cadastro();
 }
 
-function abastecimento_de_carros() {
-    let AlcoolWayneOil = ask_for_number(`Qual o valor do álcool no posto Wayne Oil`);
-    let GasolinaWayneOil = ask_for_number(`Qual o valor da gasolina no posto Wayne Oil?`);
-    let AlcoolStarkPetrol = ask_for_number(`Qual o valor do álcool no posto Stark Petrol`);
-    let GasolinaStarkPetrol = ask_for_number(`Qual o valor da gasolina no posto Stark Petrol?`);
+function ComSouZ() {
+    let ListaDeHospedes = [];
 
-    AlcoolWayneOil *= QUANTIDADE_DE_LITROS;
-    GasolinaWayneOil *= QUANTIDADE_DE_LITROS;
-    AlcoolStarkPetrol *= QUANTIDADE_DE_LITROS;
-    GasolinaStarkPetrol *= QUANTIDADE_DE_LITROS;
+    function cadastrar() {
+        if (ListaDeHospedes.length >= 15) {
+            alert(`Máximo de cadastros atingido`);
+            main();
+            return
+        }
 
-    let PostoWayneRelacaoPreco = (AlcoolWayneOil / GasolinaWayneOil);
-    let PostoStarkRelacaoPreco = (AlcoolStarkPetrol / GasolinaStarkPetrol);
-
-    let PostoEmConta = null;
-    let ModoDeCombustivel = null;
-
-    if (PostoWayneRelacaoPreco < PostoStarkRelacaoPreco) {
-        PostoEmConta = "Wayne Oil";
-        ModoDeCombustivel = PostoWayneRelacaoPreco <= 0.7 ? "álcool" : "gasolina";
-    } else {
-        PostoEmConta = "Stark Petrol";
-        ModoDeCombustivel = PostoStarkRelacaoPreco <= 0.7 ? "álcool" : "gasolina";
+        let NomeDoHospede = askForString(`Qual o nome do hóspede?`);
+        ListaDeHospedes.push(NomeDoHospede);
+        alert(`Hóspede ${NomeDoHospede} cadastrado com sucesso`);
+        main();
     }
 
-    alert(`${NomeDoUsuario}, é mais barato abastecer com ${ModoDeCombustivel} no posto ${PostoEmConta}.`);
+    function pesquisar() {
+        let NomeDoHospede = askForString(`Qual o nome do hóspede?`);
+        let Encontrado = ListaDeHospedes.includes(NomeDoHospede);
+        let FinalString = Encontrado == true ? `${NomeDoHospede} foi encontrado(a)!` : `${NomeDoHospede} não foi encontrado(a)!`;
+
+        alert(FinalString);
+        main();
+    }
+
+    function listar() {
+        let FinalString = `Lista de Hóspedes:\n`
+
+        ListaDeHospedes.forEach(index => {
+            FinalString += `${index}\n`;
+        })
+
+        alert(FinalString);
+        main();
+    }
+
+    function main() {
+        let Escolha = askForNumber(`Bem-vindo a aba de hóspedes! Por favor, escolha uma opção\n[1] Cadastro\n[2] Pesquisar\n[3] Listar\n[4] Sair`)
+
+        switch(Escolha) {
+            case 1:
+                cadastrar();
+                break;
+            case 2:
+                pesquisar();
+                break;
+            case 3:
+                listar();
+                break;
+            case 4:
+                Main();
+                break;
+            default:
+                alert(`Por favor, escolha um número entre 1 e 4`);
+                main();
+                break;
+        }
+    }
+
+    main();
+}
+
+function Error() {
+    alert(`Por favor, escolha um número entre 1 e 10`);
     Main();
 }
 
-function sair() {
-    let Confirma = confirm(`Você realmente deseja sair, ${NomeDoUsuario}?`);
-    if (!Confirma) {
-        Main();
-        return
-    }
-
-    alert(`Muito obrigado e até logo, ${NomeDoUsuario}.`)
+function Sair() {
     window.close();
-}
-
-function errror() {
-    alert(`Opção inválida, informe um número entre 1 e 4`);
-    Main();
 }
 
 Main();
